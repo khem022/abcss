e245395d7d83244bb3668e6e535c26f19f288d58
khem3 <khemrajtikapur@gmail.com>
2023-11-16 14:56:34 +0530

    hi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<html>
+  <body>
+    <h1>HI welcome to acharya institute</h1>
+  </body>
+</html>
+  );
+}
+
+export default App;
