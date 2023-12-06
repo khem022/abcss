f215991e465b73f3e5e12937d0cf0d557309cc8b
khem <khem22@gmail.com>
2023-12-06 22:13:18 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-<html>
-  <body>
-    <h1>HI welcome to acharya institute</h1>
-  </body>
-</html>
+    <html>
+      <body>
+        <h1>hello mr</h1>
+      </body>
+    </html>
   );
 }
+export default App;
 
-export default App;
