052a9f9adc4959060e121f8da212b5c8970c489e
abc <abc2@gmail.com>
2023-12-09 10:26:57 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-function App(){
-    return (
-        <html>
-            <body>
-                <h1>hi all </h1>
-            </body>
-        </html>
-    );
+function App() {
+return (
+<html>
+<body>
+<h1>hello mr</h1>
+</body>
+</html>
+);
 }
 export default App;
